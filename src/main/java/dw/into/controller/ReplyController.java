3e7192d5c6ec473d9142602aa3994f87ae36ae7b
afoisl <160198328+afoisl@users.noreply.github.com>
2024-09-03 17:50:52 +0900

    댓글 수정

--- src/main/java/dw/into/controller/ReplyController.java
+++ src/main/java/dw/into/controller/ReplyController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/reply")
@@ -19,15 +20,32 @@ public class ReplyController {
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Reply> saveReply(@RequestBody Reply reply) {
-        return new ResponseEntity<>(replyService.saveReply(reply),
-                HttpStatus.OK);
+    public ResponseEntity<ReplyDto> saveReply(@RequestBody ReplyDto replyDto) {
+        Reply reply = new Reply();
+        reply.setUser(replyDto.getUserId()); // User 객체
+        reply.setQnA(replyDto.getQnAId()); // QnA 객체
+        reply.setReplyText(replyDto.getText());
+        reply.setReplyTime(replyDto.getReplyTime());
+
+        Reply savedReply = replyService.saveReply(reply);
+
+        ReplyDto savedReplyDto = new ReplyDto();
+        savedReplyDto.setReplyId(savedReply.getReplyId());
+        savedReplyDto.setUserId(savedReply.getUser());
+        savedReplyDto.setQnAId(savedReply.getQnA());
+        savedReplyDto.setText(savedReply.getReplyText());
+        savedReplyDto.setReplyTime(savedReply.getReplyTime());
+
+        return new ResponseEntity<>(savedReplyDto, HttpStatus.OK);
     }
 
     @GetMapping("/get")
-    public ResponseEntity<List<Reply>> getAllReply() {
-        return new ResponseEntity<>(replyService.getAllReply(),
-                HttpStatus.OK);
+    public ResponseEntity<List<ReplyDto>> getAllReply() {
+        List<Reply> replies = replyService.getAllReply();
+        List<ReplyDto> replyDtos = replies.stream()
+                .map(reply -> new ReplyDto().toReplyDtoFromReply(reply))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(replyDtos, HttpStatus.OK);
     }
 
     @GetMapping("/dto")
